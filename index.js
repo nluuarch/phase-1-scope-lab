847ec762b167484e952dd96355ad30351309ebca
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-07 12:33:51 -0800

    commit tree 26cd6a1adb7914b9d762d14795af678622ce6bae

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function setBestCustomer(){
+bestCustomer = 'not bob';
+}
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+//    return customerName;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Trump';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Hitler';
+}
